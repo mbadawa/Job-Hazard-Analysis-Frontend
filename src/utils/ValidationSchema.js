e62a8e2b98b4737cfeb9cc6124f420755e4cf28d
Mohamad Badawa <7badawa@gmail.com>
2022-12-26 18:28:11 -0500

    Added frontend validation and feedback

--- src/utils/ValidationSchema.js
+++ src/utils/ValidationSchema.js
@@ -1,14 +1,14 @@
 import * as yup from 'yup';
 
 const schema = yup.object().shape({
-  // fullName: yup.string().required('Required'),
-  // superVisorName: yup.string().required('Required'),
-  // companyName: yup.string().required('Required'),
-  // projectName: yup.string().required('Required'),
-  // projectDate: yup.date().required('Required'),
-  // operation: yup.string().required('Required'),
-  // departmentOrUnit: yup.string().required('Required'),
-  // location: yup.string().required('Required'),
+  fullName: yup.string().required('Required'),
+  superVisorName: yup.string().required('Required'),
+  companyName: yup.string().required('Required'),
+  projectName: yup.string().required('Required'),
+  projectDate: yup.date().required('Required'),
+  operation: yup.string().required('Required'),
+  departmentOrUnit: yup.string().required('Required'),
+  location: yup.string().required('Required'),
 });
 
 export default schema;
